6e49fb77dcd5a0087e382cd25a63b48b807845aa
Susan K. Coulter <markus@wf-fe1.lanl.gov>
2018-04-13 08:43:39 -0600

    add OPA capability

--- xtic.sh
+++ xtic.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
         echo " "
-        echo "$0: ERROR: No file name provided"
-        echo "$0: USAGE: $0 <filename> - the filename containing route data"
+        echo "$0: ERROR: Not enough parameters provided"
+        echo "$0: USAGE: $0 <filename> <IB or OPA>"
+	echo "              - the order of the parameters must be accurate"
         echo " "
         exit
 fi
 
-lid="0"
+if [[ $2 -ne "IB" && $2 -ne "OPA" ]]
+then
+        echo "$0: ERROR: Bad transport provided, $2"
+fi
+
+# generate appropriate tics
+
+dev="0"
 tic=0
 echo -n "set xtics (" > /tmp/xtics.$$
 
@@ -20,19 +28,28 @@ do
                 tic=1
         ;;
         *)
-                lx=`echo $line | sed 's/\/.*//'`
+                dx=`echo $line | sed 's/\/.*//'`
                 px=`echo $line | sed 's/.*\///' | sed 's/ .*//'`
+		desc=$dx
 
-                if [ $lid -ne $lx ]
-                then    
-                        desc=`ibnetdiscover | grep Switch | grep "lid $lx "| awk '{print $8}' | sed 's/"//'`
-                        echo -n "\""$desc"\"" $tic"," >> /tmp/xtics.$$;
-                fi      
-                lid=$lx 
+		if [ $2 == "IB" ]
+		then
+			if [ $dev -ne $dx ]
+			then
+				desc=`ibnetdiscover | grep Switch | grep "lid $dx "| awk '{print $8}' | sed 's/"//'`
+				echo -n "\""$desc"\"" $tic"," >> /tmp/xtics.$$;
+                        fi
+		else
+			if [ $dev != $dx ]
+			then
+				echo -n "\""$desc"\"" $tic"," >> /tmp/xtics.$$;
+			fi	
+		fi
+
+                dev=$dx 
 		let "tic = $tic + 1"
         ;;
         esac
-
 done
 echo ")" >> /tmp/xtics.$$
 
